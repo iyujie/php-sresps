702eddc70bf376f44f622f707944af41ff96b92c
iyujie <leslie.hahax@hotmail.com>
2017-04-07 05:02:23 +0800

    completed-CRUD-skeleton-interface
    functions and algorithms not complete. several bugs

--- bootstrap-3.3.7-dist/js/script.js
+++ bootstrap-3.3.7-dist/js/script.js
@@ -1,5 +1,5 @@
 var app = angular.module("phpApp", ["ngRoute"]);
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
     $routeProvider.when("/Items",{
         templateUrl:"view-list.html", controller: "listController"
     })
@@ -9,16 +9,17 @@ app.config(function ($routeProvider) {
     .when("/Items/:index",{
         templateUrl:"view-details.html", controller:"editController"
     })
-    .otherwise({redirectTo:"/Items"})
-})
+    .otherwise({
+    redirectTo:"/Items"
+    })
+});
 
 app.factory("salesService", ["$rootScope", function($rootScope){
     var svc = {};
     
     var data = [
-        {id:"001", item:"phyeeiana", name:"Sana Faiz", date:"4/4/2017", price:47.50},
-        {id:"001", item:"phyeeiana", name:"Nurul", date:"4/4/2017", price:47.50},
-        {id:"001", item:"phyeeiana", name:"leslie", date:"4/4/2017", price:47.50}
+        {id:"1", item:"Xanax", name:"Sana Faiz", date:"04/06/2017", price:247.50},
+        {id:"2", item:"Viagra", name:"Nurul", date:"05/03/2017", price:198.50}
     ];
     
     svc.getSales = function()
@@ -26,8 +27,7 @@ app.factory("salesService", ["$rootScope", function($rootScope){
         return data;
     };
     
-    svc.addSales = function(sales)
-    {
+    svc.addSales = function(sales) {
         data.push(sales);
     };
     
@@ -36,56 +36,72 @@ app.factory("salesService", ["$rootScope", function($rootScope){
         data[index] = sales;
     };
     
+    svc.delSales = function(index, sales)
+    {
+        data[index] = sales;
+    };
+    
+    
     return svc;
 }]);
 
-app.controller("listController", ["$scope", "$location", "$routeParams", "salesService", function($scope, $location, $routeParams, salesService){
+app.controller("listController", ["$scope", "$location", "$routeParams", "salesService", 
+                                  function($scope, $location, $routeParams, salesService){
     $scope.data = salesService.getSales();
     
     $scope.addSales = function(){
-      $location.path("/Items/add");  
+      $location.path("/Items/add");
     };
     
     $scope.editSales = function(x)
     {
         $location.path("/Items/" + x);
     };
-    
+                                      
     $scope.delSales = function(x)
     {
-        //todo
+        if (confirm('Are you sure to delete?'))
+        {
+             alert("deleted item asdasd");
+            //var index = $scope.bdays.indexOf(item);
+            //$scope.bdays.splice(index, 1);
+        }
+        $location.path("/Items");
     };
+                                      
+    
 }]);
 
 
 
-app.controller("addController", ["$scope", "$location", "$routeParams", "salesService", function($scope, $location, $routeParams, salesService){
-    $scope.save = function()
+app.controller("addController", ["$scope", "$location", "$routeParams", "salesService", 
+                                  function($scope, $location, $routeParams, salesService){
+    $scope.saveStuff = function()
     {
-        salesService.addSales({name: $scope.Item.name, genre: $scope.Item.item});
+        salesService.addSales({id: $scope.Item.id, item: $scope.Item.item, name: $scope.Item.name, date: $scope.Item.date, price: $scope.Item.price});
         $location.path("/Items");
     };
     
-    $scope.cancel = function()
+    $scope.cancelStuff = function()
     {
         $location.path("/Items")
     };
-    
 }]);
 
-app.controller("editController", ["$scope", "$location", "$routeParams", "salesService", function($scope, $location, $routeParams, salesService){
-    
+app.controller("editController", ["$scope", "$location", "$routeParams", "salesService", 
+                                  function($scope, $location, $routeParams, salesService){  
     $scope.Item = salesService.getSales()[parseInt($routeParams.index)];
     
-    $scope.save = function()
-    {
-        salesService.addSales(parseInt($routeParams.index), {name: $scope.Item.name, genre: $scope.Item.item});
-        $location.path("/Items");
-    };
+    $scope.saveStuff = function(){salesService.addSales(parseInt($routeParams.index), {id: $scope.Item.id, item: $scope.Item.item, name: $scope.Item.name, date: $scope.Item.date, price: $scope.Item.price});
+                             $location.path("/Items");};
     
-    $scope.cancel = function()
-    {
+    $scope.cancelStuff = function(){
         $location.path("/Items")
     };
+                                      
     
 }]);
+
+
+
+
