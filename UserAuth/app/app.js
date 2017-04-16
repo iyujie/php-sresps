3ff88bb602e81a5e12a214b97b65a5cbabb3313e
swana-16 <4323904@students.swinburne.edu.my>
2017-04-16 20:24:13 +0800

    Add files via upload
    **Working User Authentication Files**

---
+++ UserAuth/app/app.js
@@ -0,0 +1,55 @@
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate', 'toaster']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+        $routeProvider.
+        when('/login', {
+            title: 'Login',
+            templateUrl: 'html/login.html',
+            controller: 'authCtrl'
+        })
+            .when('/logout', {
+                title: 'Logout',
+                templateUrl: 'html/login.html',
+                controller: 'logoutCtrl'
+            })
+            .when('/signup', {
+                title: 'Signup',
+                templateUrl: 'html/signup.html',
+                controller: 'authCtrl'
+            })
+            .when('/dashboard', {
+                title: 'Dashboard',
+                templateUrl: 'html/dashboard.html',
+                controller: 'authCtrl'
+            })
+            .when('/', {
+                title: 'Login',
+                templateUrl: 'html/login.html',
+                controller: 'authCtrl',
+                role: '0'
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
+  }])
+    .run(function ($rootScope, $location, Data) {
+        $rootScope.$on("$routeChangeStart", function (event, next, current) {
+            $rootScope.authenticated = false;
+            Data.get('session').then(function (results) {
+                if (results.uid) {
+                    $rootScope.authenticated = true;
+                    $rootScope.uid = results.uid;
+                    $rootScope.name = results.name;
+                    $rootScope.email = results.email;
+                } else {
+                    var nextUrl = next.$$route.originalPath;
+                    if (nextUrl == '/signup' || nextUrl == '/login') {
+
+                    } else {
+                        $location.path("/login");
+                    }
+                }
+            });
+        });
+    });
