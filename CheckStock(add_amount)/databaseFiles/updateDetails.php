e3bb983ee21495800f9a33ef703fcae7638fc057
Nurul-4332008 <4332008@students.swinburne.edu.my>
2017-04-18 00:07:43 +0800

    Check stock and add amount of stock

---
+++ CheckStock(add_amount)/databaseFiles/updateDetails.php
@@ -0,0 +1,15 @@
+<?php 
+// Including database connections
+require_once 'database_connections.php';
+// Fetching the updated data & storin in new variables
+$data = json_decode(file_get_contents("php://input")); 
+// Escaping special characters from updated data
+$ItemID = mysqli_real_escape_string($con, $data->ItemID);
+
+$Amount = mysqli_real_escape_string($con, $data->Amount);
+
+// mysqli query to insert the updated data
+$query = "UPDATE items SET Amount='$Amount' WHERE ItemID=$ItemID";
+mysqli_query($con, $query);
+echo true;
+?>
