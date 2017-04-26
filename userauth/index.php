2e168989d1f5d14de492c6a525af838db14585be
Sana Faiz Abdullah <4323904@students.swinburne.edu.my>
2017-04-26 21:01:13 +0800

    updated

--- userauth/index.php
+++ userauth/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-          <title>PHP INC.</title>
+          <title>AngularJS Authentication App</title>
           <!-- Bootstrap -->
           <link href="css/bootstrap.min.css" rel="stylesheet">
             <link href="css/custom.css" rel="stylesheet">
@@ -24,7 +24,7 @@
 <![endif]-->
               </head>
 
-  <body ng-cloak="">
+ <body ng-cloak="">
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="row">
