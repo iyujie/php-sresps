05ef06b72d4d8c4f97dde9d648be3ef181a5f2db
iyujie <leslie.hahax@hotmail.com>
2017-05-07 18:35:26 +0800

    added-base-code-for-particular-date-searching
    weekly, monthly and yearly are undone yet

--- predictSales.php
+++ predictSales.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +8,8 @@
     <meta name="viewport" content="width=device-width, intial-scale=1.0"/>
     <link href="https://bootswatch.com/simplex/bootstrap.min.css" rel="stylesheet"/>
     <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-    
+    <script src="bootstrap-3.3.7-dist/js/jquery.min.js"></script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
 </head>
 <body>
     <div class="container-fluid" style="width:15%; float:left; padding-top:15px; background-color:#f4f4f4;">
@@ -62,29 +66,76 @@
                     <?php
 
                         require_once 'database.php';
-
-                        $query= "SELECT ItemID, ItemName, SUM(Quantity) AS total FROM Sales GROUP BY ItemID ORDER BY total DESC LIMIT 5";
-                        $result= mysqli_query($con, $query);
-                        if ($result) {
-                            while($row = mysqli_fetch_array($result)){   
-                            echo "<tr>
-                            <td>" . $row['ItemID'] . "</td>
-                            <td>" . $row['ItemName'] . "</td>
-                            <td>" . $row['total'] . "</td>
-                            </tr>";  
-                            }
-
-                        }else
+                        
+                        if(isset($_POST['btn-save']))
                         {
-                            die('Invalid query: ' . mysqli_error());
+
+
+                            $start_date= $_POST['start_date'];
+                            $end_date= $_POST['end_date'];
+                            $query="select ItemID, ItemName, SUM(Quantity) AS total from Sales where SalesDate between '$start_date 00:00:00 and '$end_date 23:59:00' order by SalesDate desc limit 5";
+                            mysqli_query($con, $query);
+
+
+                            echo "
+                        <!DOCTYPE html>
+                        <script>
+                        function redir()
+                        {
+                        alert('Updated list.');
+                        window.location.assign('predictSales.php');
+                        }
+                        </script>
+                        <body onload='redir();'></body>";
+
+
+                        }
+                        else{
+
+                            $query= "SELECT ItemID, ItemName, SUM(Quantity) AS total FROM Sales GROUP BY ItemID ORDER BY total DESC LIMIT 5";
+                            $result= mysqli_query($con, $query);
+                            if ($result) {
+                                while($row = mysqli_fetch_array($result)){   
+                                echo "<tr>
+                                <td>" . $row['ItemID'] . "</td>
+                                <td>" . $row['ItemName'] . "</td>
+                                <td>" . $row['total'] . "</td>
+                                </tr>";  
+                                }
+
+                            }else
+                            {
+                                die('Invalid query: ' . mysqli_error());
+                            }
                         }
                     ?>
                    
                 </table>
-                <div class="form-group">
-                    <button type="submit" name="btn-save" class="btn btn-primary">Group by Week</button>
-                    <button type="submit" name="btn-save" class="btn btn-primary">Group by Month</button>
-                    <button type="submit" name="btn-save" class="btn btn-primary">Group by Year</button>
+                
+                <div class="btn-group">
+                  <a href="#" class="btn btn-primary">Group Top Sales by</a>
+                  <a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Weekly</a></li>
+                    <li><a href="#">Monthly</a></li>
+                    <li><a href="#">Yearly</a></li>
+
+                  </ul>
+                </div>
+                
+                <div class="form-group" style="width:30%">
+                  <label class="control-label">Group by Set Date</label>
+                  <div class="input-group">
+                    <span class="input-group-addon">Start Date</span>
+                    <input class="form-control" type="text" name="start_date" placeholder="yyyy/mm/dd">
+                  </div>
+                  <div class="input-group">
+                    <span class="input-group-addon">End Date</span>
+                    <input class="form-control" type="text" name="end_date" placeholder="yyyy/mm/dd">
+                    <span class="input-group-btn">
+                      <button type="submit" name="btn-save" class="btn btn-primary">Submit</button>
+                    </span>
+                  </div>
                 </div>
             </div>
         </div>
