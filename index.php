52430fed6c56f9a5a81d6405996c72d4e50f7d19
Nurul-4332008 <4332008@students.swinburne.edu.my>
2017-05-10 22:12:21 +0800

    Adding "Daily/Weekly/Monthly Profit" to the navigation bar

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
             <li><a href="listProduct.html">Add/Delete Product</a></li>
             <li><a href="listStock.html">Add/Edit Stock</a></li>
 			<li><a href="Weekly.php">Weekly/Monthly Sales</a></li>
+			<li><a href="profitDay.php">Daily/Weekly/Monthly Profit</a></li>
             <li><a href="#">Sales Graph Visual</a></li>
             <li><a href="predictSales.php">Check In-Demand Products</a></li>
           <li class="disabled"><a href="#">Disabled</a></li>
